8b6f20347e6e1cd2025d92e4de700df196bfb1c0
JAAR <joe.aaron.acosta@outlook.com>
2023-09-15 01:09:22 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,65 @@
+'''
+>>> JAAR
+>>> 09/15/2023
+>>> Practicing Fundamentals Program 23
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that takes information and creates a JSON file then writes information to the JSON file.
+'''
+
+data = [
+	{
+		'name': "Germaine Garner",
+		'phone': "(881) 391-0211",
+		'email': "fermentum.convallis.ligula@google.ca",
+		'country': "Nigeria",
+		'region': "Vienna",
+		'address': "Ap #544-2545 Vitae Rd.",
+		'list': "5"
+	},
+	{
+		'name': "Bevis Robinson",
+		'phone': "1-294-632-1467",
+		'email': "amet.massa.quisque@protonmail.ca",
+		'country': "Colombia",
+		'region': "Stockholms län",
+		'address': "531-2593 Urna. Av.",
+		'list': "17"
+	},
+	{
+		'name': "Xander Foreman",
+		'phone': "(517) 850-6135",
+		'email': "a@protonmail.net",
+		'country': "China",
+		'region': "Maranhão",
+		'address': "8096 Ut St.",
+		'list': "9"
+	},
+	{
+		'name': "Oren Macdonald",
+		'phone': "(324) 790-7184",
+		'email': "non.luctus.sit@protonmail.com",
+		'country': "Nigeria",
+		'region': "İzmir",
+		'address': "9844 Quis Ave",
+		'list': "1"
+	},
+	{
+		'name': "Regan Rush",
+		'phone': "1-219-727-3279",
+		'email': "nunc@google.ca",
+		'country': "France",
+		'region': "Baja California",
+		'address': "Ap #850-4383 Ipsum Ave",
+		'list': "19"
+	}
+]
+
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
